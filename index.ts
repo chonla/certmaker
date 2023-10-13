1b6b8c538798473ea945a20dd24b41cee593c189
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-10-13 16:06:00 -0600

    Add specific oak version

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import router from "./router.ts";
 
 const app: Application = new Application();
