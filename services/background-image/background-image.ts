a529297f0ef6abd8df585a2e75bca1c02d47ff9f
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-12-15 16:01:59 +0700

    test: Log

--- services/background-image/background-image.ts
+++ services/background-image/background-image.ts
@@ -2,6 +2,8 @@ export class BackgroundImage {
     async resolve(imageUrl: string): Promise<Uint8Array> {
         if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
             const imageDataResponse = await fetch(imageUrl);
+
+            console.log(imageDataResponse);
             const imageDataBinary = await imageDataResponse.arrayBuffer();
             const imageUint8Array = new Uint8Array(imageDataBinary);
             return imageUint8Array;
