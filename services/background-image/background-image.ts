90a68a1d695aa02595a8c1c4247cff11b8e80330
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-12-15 16:06:56 +0700

    test: URL

--- services/background-image/background-image.ts
+++ services/background-image/background-image.ts
@@ -3,7 +3,7 @@ export class BackgroundImage {
         console.log(imageUrl);
         if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
             const imageDataResponse = await fetch(imageUrl);
-
+            console.log(imageDataResponse);
             const imageDataBinary = await imageDataResponse.arrayBuffer();
             const imageUint8Array = new Uint8Array(imageDataBinary);
             return imageUint8Array;
