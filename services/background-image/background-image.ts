243bb08eea6fc7c590f46eccb2b0d054895f98ae
Chonlasith Jucksriporn <chonlasith@gmail.com>
2023-12-15 16:18:18 +0700

    fix: Self ref background

--- services/background-image/background-image.ts
+++ services/background-image/background-image.ts
@@ -1,9 +1,12 @@
 export class BackgroundImage {
     async resolve(imageUrl: string): Promise<Uint8Array> {
-        console.log(imageUrl);
+        const selfReferenceBase = 'https://certmaker.deno.dev/templates/';
+        if (imageUrl.startsWith(selfReferenceBase)) { // self reference
+            const actualImageUrl = imageUrl.substring(selfReferenceBase.length);
+            return await Deno.readFile(`templates/${actualImageUrl}.png`);
+        }
         if (imageUrl.startsWith('http://') || imageUrl.startsWith('https://')) {
             const imageDataResponse = await fetch(imageUrl);
-            console.log(imageDataResponse);
             const imageDataBinary = await imageDataResponse.arrayBuffer();
             const imageUint8Array = new Uint8Array(imageDataBinary);
             return imageUint8Array;
